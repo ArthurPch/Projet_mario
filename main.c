3b613f47f51080626d8d082126e1820bb31b93f1
Arthur <arthur.pocholle@gmail.com>
2025-01-24 15:39:30 +0100

    normalement c bon

--- main.c
+++ main.c
@@ -7,14 +7,11 @@ int main() {
         return 1;
     }
 
-<<<<<<< HEAD
     menu();
-=======
     int positionJoueur = 55; 
 
     afficherPaysage(fichier, positionJoueur);
 
     fclose(fichier);
->>>>>>> 6fab25637bf4e928bec90c0412e41093c72ad29f
     return 0;
 }
