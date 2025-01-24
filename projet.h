268bcee7e7bc639c5bdd97fa441cf19cd4d8c9d4
Arthur <arthur.pocholle@gmail.com>
2025-01-24 10:50:40 +0100

    initialisations des premieres fonctions dans le .h

---
+++ projet.h
@@ -0,0 +1,37 @@
+#ifndef projet.h
+#define projet.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <string.h>
+
+void afficherPaysage(FILE *fichier, int positionJoueur);
+
+typedef struct {
+    int positionX;
+    int positionY;
+    int saut;
+    int enjeu;
+} Personnage;
+
+void cacherCurseur();
+
+void positionnerCurseur(int x, int y);
+
+void initialiserPersonnage(Personnage* perso);
+
+void afficherGrille(Personnage* perso);
+
+void avancer(Personnage* perso);
+
+void reculer(Personnage* perso);
+
+void sauter(Personnage* perso, int direction);
+
+void quitterJeu(Personnage* perso);
+
+void deplacement(Personnage* perso);
+
+#endif 
