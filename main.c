5a3373209ff083e1e0f3606168b18e0f28a81b67
Raphael <raphael.souchet.85@gmail.com>
2025-01-24 11:47:48 +0100

    affichage du paysage

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include "projet.h"
 
-int main(){
+int main() {
+    FILE *fichier = fopen("Mario.txt", "r");
+    if (fichier == NULL) {
+        printf("Impossible d'ouvrir le fichier Mario.txt\n");
+        return 1;
+    }
 
-    
+    int positionJoueur = 55; 
+
+    afficherPaysage(fichier, positionJoueur);
+
+    fclose(fichier);
     return 0;
 }
